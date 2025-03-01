3cce2762d475288f2cc3121ec8efb66b4dae2c43
sonnt <thanhson.rf@gmail.com>
2025-03-01 15:05:35 +0700

    Refactor ConvertToBytes function to accept variadic bigEndian parameter and enhance data type handling

--- sutils/data.go
+++ sutils/data.go
@@ -3,13 +3,16 @@ package sutils
 import (
 	"bytes"
 	"encoding/binary"
-	"errors"
 	"reflect"
 )
 
 // ConvertToBytes converts any struct to a byte slice
-func ConvertToBytes(data any, bigEndian bool) ([]byte, error) {
+func ConvertToBytes(data any, bigEndians ...bool) ([]byte, error) {
 	var buf bytes.Buffer
+	bigEndian := false
+	if len(bigEndians) > 0 {
+		bigEndian = bigEndians[0]
+	}
 	err := binaryWrite(&buf, data, bigEndian)
 	if err != nil {
 		return nil, err
@@ -23,9 +26,6 @@ func binaryWrite(buf *bytes.Buffer, data any, bigEndian bool) error {
 	if v.Kind() == reflect.Ptr {
 		v = v.Elem()
 	}
-	if v.Kind() != reflect.Struct {
-		return errors.New("data is not a struct or pointer to struct")
-	}
 
 	var order binary.ByteOrder
 	order = binary.LittleEndian
@@ -33,16 +33,27 @@ func binaryWrite(buf *bytes.Buffer, data any, bigEndian bool) error {
 		order = binary.BigEndian
 	}
 
-	for i := 0; i < v.NumField(); i++ {
-		field := v.Field(i)
-		if field.Kind() == reflect.Struct {
-			if err := binaryWrite(buf, field.Addr().Interface(), bigEndian); err != nil {
+	switch v.Kind() {
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			field := v.Field(i)
+			if err := binaryWrite(buf, field.Interface(), bigEndian); err != nil {
 				return err
 			}
-		} else {
-			if err := binary.Write(buf, order, field.Interface()); err != nil {
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			if err := binaryWrite(buf, v.Index(i).Interface(), bigEndian); err != nil {
 				return err
 			}
+		}
+	case reflect.String:
+		if err := binary.Write(buf, order, []byte(v.String())); err != nil {
+			return err
+		}
+	default:
+		if err := binary.Write(buf, order, data); err != nil {
+			return err
 		}
 	}
 	return nil
